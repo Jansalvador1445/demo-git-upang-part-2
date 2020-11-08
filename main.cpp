ba9a945fbfe3297b45c57ad3b4ac77d3949eb660
Jan Salvador Sebastian <jansalvadorsebastian@gmail.com>
2020-11-08 18:49:06 +0800

    Removed Codes

--- main.cpp
+++ main.cpp
@@ -3,16 +3,5 @@ using namespace std;
 
 int main() {
 
-    int inputInteger = 0, counterInteger = 0;
-
-    cout << "Enter number:";
-    cin >>  inputInteger;
-
-    while (inputInteger != 0){
-        counterInteger++;
-        inputInteger = inputInteger / 10;
-    }
-    
-    cout << "\nTotal numbers of digits: " << counterInteger;
 
 }
